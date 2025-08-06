ed88368aa664f46e19638cf48c38162eb4bb8174
Mainamanhoon <gour.aman07@gmail.com>
2025-08-07 01:55:39 +0530

    i am dumb eough to push runtime.ex

---
+++ config/runtime.exs
@@ -0,0 +1,47 @@
+import Config
+require Logger
+
+# config/runtime.exs is executed for all environments, including
+# during releases. It is executed after compilation and before the
+# system starts, so it is typically used to load production configuration
+# and secrets from environment variables or elsewhere. Do not define
+# any compile-time configuration in here, as it won't be applied.
+# The block below contains prod specific runtime configuration.
+
+
+
+# ## Using releases
+#
+# If you use `mix release`, you need to explicitly enable the server
+# by passing the PHX_SERVER=true when you start it:
+#
+#     PHX_SERVER=true bin/shader_backend start
+#
+# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
+# script that automatically sets the env var above.
+
+
+if config_env() == :prod do
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
+  config :shader_backend, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+
+  config :shader_backend, ShaderBackendWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      # Bind to all interfaces on the specified port
+      ip: {0, 0, 0, 0},
+      port: port
+    ],
+    secret_key_base: secret_key_base
+    server:true
+end
